5652eb1e54b5fa34ca3065691f19911e90612fb5
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2025-09-12 09:51:24 -0300

    feat: adds repository interfaces

---
+++ UserRegistration.Domain/Interfaces/IRepositories/IProfileRepository.cs
@@ -0,0 +1,9 @@
+﻿using UserRegistration.Domain.Entities;
+
+namespace UserRegistration.Domain.Interfaces.IRepositories
+{
+    public interface IProfileRepository
+    {
+        Task<Profile> GetProfileById(int id);
+    }
+}
---
+++ UserRegistration.Domain/Interfaces/IRepositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using UserRegistration.Domain.Entities;
+
+namespace UserRegistration.Domain.Interfaces.IRepositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByCpf(string cpf);
+        Task<IEnumerable<User>> GetUsersByProfile(int idProfile);
+        Task<User> GetById(Guid id);
+        Task Create(User user);
+        Task Update(User user);
+        Task Delete(Guid id);
+        Task<bool> CheckIfCpfAlreadyUsed(string cpf, Guid? userId = null);
+    }
+}
